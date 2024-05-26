ee61daeb811c9b5592a3819d7bab0bb22af8b7d7
Lisovfox <160726738+Lisovfox@users.noreply.github.com>
2024-05-26 14:54:42 +0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,15 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+order = {
+    "firstName": "Andrey",
+    "lastName": "Lisov",
+    "address": "Moscow",
+    "metroStation": 4,
+    "phone": "+7 916 111 22 33",
+    "rentTime": 1,
+    "deliveryDate": "2024-06-06",
+    "comment": "Hello",
+    "color": ["BLACK"]
+}
